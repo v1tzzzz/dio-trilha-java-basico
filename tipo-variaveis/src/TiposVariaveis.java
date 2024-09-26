e27a3944626749137d7488c3c1792f54e13665d7
Alysson Vitor <aly.vtzcode@gmail.com>
2024-09-26 17:03:20 -0300

    tipos primitivos

--- tipo-variaveis/src/TiposVariaveis.java
+++ tipo-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,28 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // cast em java
+        // cast e esse (short) que fala pro java converter um int em short
+        // nao e comum fazer isso
+        short numeroCurto = 1;
+        int numeroInteiro = numeroCurto;
+        short numeroPequeno = (short) numeroInteiro;
+
+        // tipos primitivos numericas em java variaveis sao
+        byte numeroBemPequeno;
+        short numeroPequeno1;
+        int numeroNormal;
+        long numeroGrande;
+
+        // Numeros fracionados
+        float numeroFracionadoTipo1;
+        double numeroFracionadoTipo2;
+
+        // Booleano a variavel que representa >True< ou >False<
+        boolean verdadeiro;
+
+        // e temos o char que e 1 carctere 
+        char a;
+
+
     }
 }
